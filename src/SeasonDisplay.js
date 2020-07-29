27a05f22f220e9bfdae3a8d15097e07bb60f188b
raksh543 <rakshitajain777@gmail.com>
2020-07-29 11:55:27 +0530

    component lifecycles and styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,51 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    Summer: {
+        text: 'Let\'s hit the beach',
+        iconName: 'sun'
+    },
+    Winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if(month>2 && month<9){
+        return lat>0 ? 'Summer' : 'Winter'; //javascript ternary expression
+    }else{
+        return lat>0 ? 'Winter' : 'Summer'; //javascript ternary expression
+    }
+}
+
+const SeasonDisplay = props => {
+
+    //Jan=0, dec=11
+    // april to sept is Summer
+    //Otherwise winter
+
+    const Season = getSeason(props.lat, new Date().getMonth());
+    //either we can use simple if else statements or we can use js ternary expression as follows
+
+    // const text = Season === 'Winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach';
+    // //one method
+    // const icon = Season == 'Winter' ? <i class="snowflake icon"></i> : <i class="sun icon"></i>;
+
+    // //second method
+    // const iconImg = Season === 'Winter' ? 'snowflake' : 'sun';
+
+    //destructuring
+    const{text, iconName} = seasonConfig[Season]
     return(
-        <div>Season</div>
+        
+        <div className={`season-display ${Season}`}>
+            <i  className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            {/* {icon} */}
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 // const App =() =>{
 //     window.navigator.geolocation.getCurrentPosition(
@@ -36,18 +38,33 @@ class App extends React.Component{
             }   //failure callback
         );
     }
-    render(){
+
+    //a helper method to wrap content in borders
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>
         }else if(this.state.lat && !this.state.errorMessage){
-        return <div>Latitude: {this.state.lat}</div>
-        }else return <div>Loading..</div>
+        // return <div>Latitude: {this.state.lat}</div>
+        return <SeasonDisplay lat={this.state.lat} />
+        }else{ 
+            return (
+                <Spinner message="Please accept location request" />
+            );
+        };
         
         // return(
         //     <div>
         //         Latitude:{this.state.lat} {this.state.errorMessage}
         //     </div>
         // );
+    }
+
+    render(){
+        return(
+       <div classNamr="border red">
+           {this.renderContent()}
+        </div>
+        );
     };
 };
 
